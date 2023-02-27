29894d5d498e4c7a23598b1f2a3439c8937756e2
BibinJeevan <125947048+BibinJeevan@users.noreply.github.com>
2023-02-27 23:27:43 +0530

    Cricket Players Array Stored in Html Table

---
+++ ArrayHtmlTable.php
@@ -0,0 +1,22 @@
+<html>
+<?php
+$players=array(
+    array('id'=>1,'name'=>'Sachin'),
+    array('id=>2,'name'=>'Dhoni'),
+    array('id=>3,'name'=>'Virat'),
+    array('id=>4,'name'=>'Rohit'),
+   );
+echo '<table border="1">';
+foreach($players as $player array)
+ {
+  echo  '<td>';
+ foreach($playerarray as $val)
+ {
+   echo '<td>'.$val.'</td>';
+}
+ echo '<tr>';
+}
+echo "</table>";
+?>
+</html>
+ 
